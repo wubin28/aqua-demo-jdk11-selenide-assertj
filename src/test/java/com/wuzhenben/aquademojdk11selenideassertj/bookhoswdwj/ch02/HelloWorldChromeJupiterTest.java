8ab4546edd4898db2f630d285f708ea3461c6be5
Ben Wu <wubin28@gmail.com>
2023-09-18 09:48:27 +0800

    docs: add the purpose of the project in README.md

--- src/test/java/com/wuzhenben/aquademojdk11selenideassertj/bookhoswdwj/ch02/HelloWorldChromeJupiterTest.java
+++ src/test/java/com/wuzhenben/aquademojdk11selenideassertj/bookhoswdwj/ch02/HelloWorldChromeJupiterTest.java
@@ -1,4 +1,4 @@
-package com.wuzhenben.aquademojdk11selenideassertj;
+package com.wuzhenben.aquademojdk11selenideassertj.bookhoswdwj.ch02;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
